1c1072be5b7b03d25c49594eeba6819d3569d9a6
Marcelo Martins <marcelo.a.martins@hotmail.com>
2021-06-28 11:01:36 -0300

    teste
    teste

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, send
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('status')
+def statusConexao(data):
+    print('status', data) 
+
+@socketio.on('enviarmsg')
+def postmsg(msg):
+    emit('enviarmsg', msg, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
